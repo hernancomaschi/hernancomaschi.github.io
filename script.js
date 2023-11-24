db0c673b948e6c1b3dddc5dedbf962bf77fc81e1
hernancomaschi <hernan.comaschi@outlook.com>
2023-11-24 13:16:30 -0600

    Final

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* 
-   Lakeland Reeds Bed & Breakfast style sheet 
-   Filename: script.js
+Lakeland Reeds Bed & Breakfast style sheet 
+Filename: script.js
 
-   Author:   Hernan Comaschi
-   Date:     2023
-   HTML5 and CSS3 Illustrated Unit M, Lessons
- */
+Author:   Hernan Comaschi
+Date:     2023
+HTML5 and CSS3 Illustrated Unit M, Lessons
+*/
 
 var navButton = document.querySelector(".navigation-menu-button img");
 if (navButton.addEventListener) {
@@ -36,43 +36,43 @@ var submitButton = document.querySelector(".submitbutton");
 
 /* add text to preview section */
 function completePreview() {
-    fromValue.textContent = nameField.value;
-    emailValue.textContent = emailField.value;
-    phoneValue.textContent = phoneField.value;
-    feedbackValue.textContent =  feedbackField.value;
-    feedbackPreview.className = "feedback-preview show";
-    if (form.checkValidity() === true) {
-        submitButton.className = "submitbutton show";
-    }
+   fromValue.textContent = nameField.value;
+   emailValue.textContent = emailField.value;
+   phoneValue.textContent = phoneField.value;
+   feedbackValue.textContent =  feedbackField.value;
+   feedbackPreview.className = "feedback-preview show";
+   if (form.checkValidity() === true) {
+      submitButton.className = "submitbutton show";
+   }
 }
 
 // A function to format text to look like a phone number
 function phoneFormat(input){
-    // Strip all characters from the input except digits
-    input = input.replace(/\D/g,'');
-    
-    // Trim the remaining input to ten characters, to preserve phone number format
-    input = input.substring(0,10);
-
-    // Based upon the length of the string, we add formatting as necessary
-    var size = input.length;
-    if(size == 0){
-            input = input;
-    }else if(size < 4){
-            input = '('+input;
-    }else if(size < 7){
-            input = '('+input.substring(0,3)+') '+input.substring(3,6);
-    }else{
-            input = '('+input.substring(0,3)+') '+input.substring(3,6)+' - '+input.substring(6,10);
-    }
-    return input; 
+   // Strip all characters from the input except digits
+   input = input.replace(/\D/g,'');
+   
+   // Trim the remaining input to ten characters, to preserve phone number format
+   input = input.substring(0,10);
+   
+   // Based upon the length of the string, we add formatting as necessary
+   var size = input.length;
+   if(size == 0){
+      input = input;
+   }else if(size < 4){
+      input = '('+input;
+   }else if(size < 7){
+      input = '('+input.substring(0,3)+') '+input.substring(3,6);
+   }else{
+      input = '('+input.substring(0,3)+') '+input.substring(3,6)+' - '+input.substring(6,10);
+   }
+   return input; 
 }
 
 /* create event listener for changes to from */
 form.addEventListener("change", completePreview, false); 
 
 document.getElementById('phoneinput').addEventListener('keyup',function(evt){
-    var phoneNumber = document.getElementById('phoneinput');
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
-    phoneNumber.value = phoneFormat(phoneNumber.value);
+   var phoneNumber = document.getElementById('phoneinput');
+   var charCode = (evt.which) ? evt.which : evt.keyCode;
+   phoneNumber.value = phoneFormat(phoneNumber.value);
 });
