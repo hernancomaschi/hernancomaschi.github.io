0987f75887aaf414648c5a606e4d24a4c556d908
hernancomaschi <hernan.comaschi@outlook.com>
2023-11-24 11:05:10 -0600

    Change in services

--- script.js
+++ script.js
@@ -7,6 +7,18 @@
    HTML5 and CSS3 Illustrated Unit M, Lessons
  */
 
+var navButton = document.querySelector(".navigation-menu-button img");
+if (navButton.addEventListener) {
+   navButton.addEventListener("click", function() {
+      var nav = document.querySelector(".sitenavigation ul");
+      if (nav.style.display === "block") {
+         nav.style.display = "";
+      } else {
+         nav.style.display = "block";
+      }
+   }, false);
+}
+
 /* create variables */
 var fromValue = document.querySelector(".feedback-from");
 var emailValue = document.querySelector(".feedback-email");
